8a36fdd0f28a6cffb39640a35f1c4360447bb317
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2024-10-06 16:09:37 +0600

    added navbar and hero section for car business

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "./App.css";
+
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
